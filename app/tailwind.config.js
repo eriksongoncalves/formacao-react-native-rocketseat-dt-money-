a37c0fee9f9b81a0bf76f1a47fe271cca99f6e36
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-07-19 13:41:00 -0300

    feat: config nativewind

---
+++ app/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: [
+    "./App.tsx",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/screens/**/*.{ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
