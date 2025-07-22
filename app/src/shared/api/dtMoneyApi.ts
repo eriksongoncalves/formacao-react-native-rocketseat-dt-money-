54f9d4cfcd787e389d38e1c590b7b9ca270b8432
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-07-22 20:13:50 -0300

    feat: add interceptors

--- app/src/shared/api/dtMoneyApi.ts
+++ app/src/shared/api/dtMoneyApi.ts
@@ -1,5 +1,9 @@
-import axios from "axios";
+import axios, { InternalAxiosRequestConfig } from "axios";
 import { Platform } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AppError } from "../helpers/AppError";
+import { IAuthenticateResponse } from "../interfaces/https/authenticate-response";
 
 const baseURL = Platform.select({
   ios: "http://localhost:3001",
@@ -9,3 +13,32 @@ const baseURL = Platform.select({
 export const dtMoneyApi = axios.create({
   baseURL,
 });
+
+dtMoneyApi.interceptors.request.use(async (config) => {
+  const userData = await AsyncStorage.getItem("dt-money-user");
+
+  if (userData) {
+    const { token } = JSON.parse(userData) as IAuthenticateResponse;
+
+    if (!config.headers) {
+      config.headers = {} as InternalAxiosRequestConfig["headers"];
+    }
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
+});
+
+dtMoneyApi.interceptors.response.use(
+  (config) => config,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message));
+    } else {
+      return Promise.reject(new AppError("Falha na requisição"));
+    }
+  }
+);
