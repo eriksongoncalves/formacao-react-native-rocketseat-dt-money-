d233130dfc9d6d15a1eca231a41064f0731fdeaa
Erikson Gonçalves Sanchez <erikson.sanchez@outlook.com>
2025-07-22 14:38:45 -0300

    feat: register form

---
+++ app/src/screens/Register/RegisterForm/index.tsx
@@ -0,0 +1,104 @@
+import { ActivityIndicator, Text, View } from "react-native";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { AppButton } from "@/components/AppButton";
+import { AppInput } from "@/components/AppInput";
+import { PublicStackParamsList } from "@/routes/PublicRoutes";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { colors } from "@/shared/colors";
+import { schema } from "./schema";
+
+export interface FormRegisterParams {
+  email: string;
+  name: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export const RegisterForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<FormRegisterParams>({
+    defaultValues: {
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+    },
+    resolver: yupResolver(schema),
+  });
+
+  const navigation = useNavigation<NavigationProp<PublicStackParamsList>>();
+
+  const onSubmit = async (userData: FormRegisterParams) => {
+    try {
+      console.log(">>> userData", userData);
+    } catch (error) {
+      console.log(">>> error", error);
+    }
+  };
+
+  return (
+    <>
+      <AppInput
+        control={control}
+        name="name"
+        placeholder="Seu nome"
+        lable="NOME"
+        leftIconName="person"
+      />
+
+      <AppInput
+        control={control}
+        name="email"
+        lable="EMAIL"
+        leftIconName="mail-outline"
+        placeholder="mail@example.br"
+      />
+
+      <AppInput
+        control={control}
+        name="password"
+        lable="SENHA"
+        placeholder="Sua senha"
+        leftIconName="lock-outline"
+        secureTextEntry
+      />
+
+      <AppInput
+        control={control}
+        name="confirmPassword"
+        lable="SENHA"
+        leftIconName="lock-outline"
+        placeholder="Confirme sua senha"
+        secureTextEntry
+      />
+
+      <View className="flex-1 justify-between mt-8 mb-6 min-h-[250px]">
+        <AppButton onPress={handleSubmit(onSubmit)} iconName="arrow-forward">
+          {!isSubmitting ? (
+            <ActivityIndicator color={colors.white} />
+          ) : (
+            "Cadastrar"
+          )}
+        </AppButton>
+
+        <View>
+          <Text className="mb-6 text-gray-300 text-base">
+            Já possui uma conta?
+          </Text>
+          <AppButton
+            onPress={() => navigation.navigate("Login")}
+            iconName="arrow-forward"
+            mode="outline"
+          >
+            Acessar
+          </AppButton>
+        </View>
+      </View>
+    </>
+  );
+};
--- app/src/screens/Register/index.tsx
+++ app/src/screens/Register/index.tsx
@@ -1,21 +1,16 @@
-import { Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
+import { View } from "react-native";
 
-import { PublicStackParamsList } from "@/routes/PublicRoutes";
-
-type NavigationProps = StackNavigationProp<PublicStackParamsList>;
+import { AuthHeader } from "@/components/AuthHeader";
+import { DismissKeyboardView } from "@/components/DismissKeyboardView";
+import { RegisterForm } from "./RegisterForm";
 
 export const Register = () => {
-  const navigation = useNavigation<NavigationProps>();
-
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Tela de Register!</Text>
-
-      <TouchableOpacity onPress={navigation.goBack}>
-        <Text>Voltar</Text>
-      </TouchableOpacity>
-    </View>
+    <DismissKeyboardView>
+      <View className="flex-1 w-[82%] self-center">
+        <AuthHeader />
+        <RegisterForm />
+      </View>
+    </DismissKeyboardView>
   );
 };
